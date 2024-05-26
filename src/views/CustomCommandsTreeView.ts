d309c33f838adb5df798e41f5ab02b16a3b0a18b
Thanh Minh <thanhdoantranminh@gmail.com>
2024-05-26 17:54:26 +0700

    feat: add custom commands tree view
    - Introduce the `CustomCommandsTreeView` class to display a tree view of the user's custom commands
    - Leverage the `CustomCommandService` to fetch and manage the custom commands
    - Render each custom command as a tree item with the command ID and description
    - Implement the `getTreeItem` and `getChildren` methods to populate the tree view
    - Add a `refresh` method to trigger updates to the tree view when the custom commands change

---
+++ src/views/CustomCommandsTreeView.ts
@@ -0,0 +1,47 @@
+import * as vscode from 'vscode'
+import { CustomCommandService } from '../services/customCommand.service'
+
+interface CommandTreeItem extends vscode.TreeItem {
+  commandId: string
+}
+
+export class CustomCommandsTreeView implements vscode.TreeDataProvider<CommandTreeItem> {
+  private _onDidChangeTreeData: vscode.EventEmitter<CommandTreeItem | undefined | void> =
+    new vscode.EventEmitter<CommandTreeItem | undefined | void>()
+  readonly onDidChangeTreeData: vscode.Event<CommandTreeItem | undefined | void> =
+    this._onDidChangeTreeData.event
+
+  private customCommandService: CustomCommandService
+  private commands: { [id: string]: { description: string } } = {}
+
+  constructor() {
+    this.customCommandService = CustomCommandService.getInstance()
+    this.customCommandService.onDidChangeCommands(() => this.refresh())
+  }
+
+  getTreeItem(element: CommandTreeItem): vscode.TreeItem {
+    return element
+  }
+
+  async getChildren(element?: CommandTreeItem): Promise<CommandTreeItem[]> {
+    if (!element) {
+      this.commands = this.customCommandService.getCommands()
+
+      return Object.keys(this.commands).map(
+        commandId =>
+          ({
+            label: commandId,
+            tooltip: this.commands[commandId].description,
+            commandId: commandId,
+            collapsibleState: vscode.TreeItemCollapsibleState.None,
+            iconPath: new vscode.ThemeIcon('terminal-bash')
+          }) as CommandTreeItem
+      )
+    }
+    return []
+  }
+
+  refresh(): void {
+    this._onDidChangeTreeData.fire()
+  }
+}
