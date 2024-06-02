9a582a21c7ee0200096222a829d3d93f84b99d35
Thanh Minh <thanhdoantranminh@gmail.com>
2024-06-02 16:50:17 +0700

    feat: add Prettier and no-explicit-any ESLint rule
    - Add the `eslint-plugin-prettier` and `eslint-config-prettier` packages to the project
    - Update the `.eslintrc.cjs` file to include the `prettier` plugin and the `@typescript-eslint/no-explicit-any` rule
    - Add a `.prettierrc` file with the project's Prettier configuration

--- src/webviews/.eslintrc.cjs
+++ src/webviews/.eslintrc.cjs
@@ -8,11 +8,12 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-explicit-any': 'warn',
   },
 }
