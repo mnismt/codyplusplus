7c5d01b9b90895c6682ff3dcaef429d85e7ef0ac
Thanh Minh <thanhdoantranminh@gmail.com>
2024-06-20 14:13:21 +0700

    feat(extension): add getCodyJsonPath function
    - Imports `path` and `vscode` modules
    - Adds a `getCodyJsonPath` function that returns the path to the `.vscode/cody.json` file

--- src/constants/cody.ts
+++ src/constants/cody.ts
@@ -1,3 +1,6 @@
+import * as path from 'path'
+import * as vscode from 'vscode'
+
 export const CODY_COMMAND = {
   MENTION: {
     FILE: 'cody.mention.file'
@@ -5,3 +8,14 @@ export const CODY_COMMAND = {
 }
 
 export const CODY_CUSTOM_COMMANDS_FILE = 'cody.json'
+
+export function getCodyJsonPath(): string | undefined {
+  const workspaceFolders = vscode.workspace.workspaceFolders
+  if (!workspaceFolders) {
+    console.error('CODY++: No workspace folder is open.')
+    return undefined
+  }
+
+  const vscodeFolderPath = path.join(workspaceFolders[0].uri.fsPath, '.vscode')
+  return path.join(vscodeFolderPath, CODY_CUSTOM_COMMANDS_FILE)
+}
